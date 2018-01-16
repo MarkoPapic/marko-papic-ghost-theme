fcadf10e300535f3303ae6fa8f2323f746e857af
Marko Papic <marko@markopapic.com>
2018-01-16 15:54:46 +0100

    Added footer and comments into consideration when showing post progress bar.

--- src/js/post-progress-bar.js
+++ src/js/post-progress-bar.js
@@ -4,7 +4,7 @@ var postProgressBar = (function () {
 
     var update = function () {
         var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight - 360;
         var scrolled = (winScroll / height) * 100;
         document.getElementById("progress-bar").style.width = scrolled + "%";
     };
